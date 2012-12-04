5c2f0484c30ae519b9621855c763c7242de27ecd
alario <alario@sina.cn>
2012-12-04 15:36:48 +0800

    22

---
+++ dujia-java/WEB-INF/src/listeners/ContextListener.java
@@ -0,0 +1,18 @@
+package listeners;
+import javax.servlet.*;
+
+public class ContextListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ dujia-java/WEB-INF/src/servlets/CaptchaServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CaptchaServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 140932425724477562L;
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	
+	
+}
