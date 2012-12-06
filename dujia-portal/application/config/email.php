4c054c304f1f2b571638144c3049b4e3599e69d1
alario <alario@sina.cn>
2012-12-06 16:35:54 +0800

    3

---
+++ dujia-portal/application/config/email.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.126.com';
+$config['smtp_user'] = 'alario';
+$config['smtp_pass'] = '9830tcli';
+
+
+/*
+ * protocol	mail	mail, sendmail, or smtp	The mail sending protocol.
+ * smtp_host	No Default	None	SMTP Server Address.
+ * smtp_user	No Default	None	SMTP Username.
+ * smtp_pass	No Default	None	SMTP Password.
+ * smtp_port	25	None	SMTP Port.
+ * smtp_timeout	5	None	SMTP Timeout (in seconds).
+ * mailtype	text	text or html	Type of mail. If you send HTML email you must send it as a complete web page. Make sure you don't have any relative links or relative image paths otherwise they will not work.
+ * 
+ */
